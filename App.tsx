19b35fd2cd7703e45b68b720addec5b8d34bb0a3
NEVI0 <neviocostamagagnin@gmail.com>
2021-04-21 10:58:23 -0300

    Initial project config

--- App.tsx
+++ App.tsx
@@ -1,21 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
+import 'react-native-gesture-handler';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import * as Font from 'expo-font';
+import AppLoading from 'expo-app-loading';
+
+import { AppProvider } from './src/contexts/AppContext';
+
+import Routes from './src/routes';
+
+const App: React.FC = () => {
+
+	const [ loaded ] = Font.useFonts({
+		'default-r': require('./assets/fonts/Nunito-Regular.ttf'),
+		'default-b': require('./assets/fonts/Nunito-Bold.ttf'),
+		'custom': require('./assets/fonts/Audiowide-Regular.ttf')
+	});
+
+	if (!loaded) {
+		return <AppLoading autoHideSplash={ true } />;
+	}
+
+	return (
+		<AppProvider>
+			<Routes />
+		</AppProvider>
+	);
+
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
